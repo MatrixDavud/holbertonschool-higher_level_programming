226e66a02176deafb9a90490d8112d2b525ace39
Davud <11396@holbertonstudents.com>
2025-06-13 19:02:36 +0400

    8th task

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in range (len.str):
+        if ord(str[i]) >= 97 and ord(str[i]) <= 122:
+            str[i] = str[i] + 26
+        else:
+            continue
+print(str)
