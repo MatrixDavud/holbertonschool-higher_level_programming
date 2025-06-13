7a82fb92fee4d8f5490e171c14036402b82557c4
Davud <11396@holbertonstudents.com>
2025-06-13 19:33:24 +0400

    11th task v2

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -2,8 +2,13 @@
 def pow(a, b):
     if b == 0:
         return 1
-    else:
+    elif b > 0:
         temp = a
         for i in range (b-1):
             a *= temp
         return a
+    else:
+        temp = a
+        for i in range (-(b)-1):
+            a *= temp
+    return 1/a
