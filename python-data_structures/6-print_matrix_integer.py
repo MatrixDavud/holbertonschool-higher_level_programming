0d92611da7f2713ebe5976fa972337d7ff035148
Davud <11396@holbertonstudents.com>
2025-06-17 23:07:00 +0400

    6th task

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for i in range(len(matrix[0])):
+        if i == 0:
+            pass
+        else:
+            print()
+        for j in range(len(matrix[0])):
+            print("{:d}".format(matrix[i][j]), end=" ")
+    print()
