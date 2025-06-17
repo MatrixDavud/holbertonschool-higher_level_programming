2de610a973df55a95b172083e8be75af713c43c1
Davud <11396@holbertonstudents.com>
2025-06-17 18:55:20 +0400

    2nd task

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print("0 arguments.")
+    elif len(sys.argv) == 2:
+        print("1 argument:")
+        print("1: {}".format(sys.argv[1]))
+    else:
+        print("{} arguments:".format(len(sys.argv)))
+        for index, value in enumerate(sys.argv):
+            print(f"{index+1}: {value}")
