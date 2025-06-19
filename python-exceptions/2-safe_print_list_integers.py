a0db8c7e4efcfd78915b2719fb1e294b4dbc7957
Davud <11396@holbertonstudents.com>
2025-06-19 14:55:44 +0400

    2nd task v2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -5,6 +5,6 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[x]))
             count += 1
-        except TypeError:
+        except (TypeError, ValueError):
             pass
     return count
