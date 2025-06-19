8ff1a004dd6e548c21886435ffc1ad14a17d8c29
Davud <11396@holbertonstudents.com>
2025-06-19 19:06:37 +0400

    Advanced task v1

---
+++ python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except (NameError, TypeError, ValueError) as e:
+        print(f"Error occurred: {e}", file=sys.stderr)
+        return False
