20fe3115d355c7daab870ec7b39bbf8f23a9bf64
Davud <11396@holbertonstudents.com>
2025-06-21 17:37:43 +0400

    pls work

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,9 +2,9 @@
 count = 122
 while count > 96:
     if count % 2 == 0:
-        print(chr(count), end='')
+        print({}.format(chr(count)), end='')
         count -= 1
     elif count % 2 == 1:
-        print(chr(count - 32), end='')
+        print({}.format(chr(count - 32)), end='')
         count -= 1
 
