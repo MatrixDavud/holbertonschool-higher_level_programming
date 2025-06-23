bc5d0b3907d3ed6d5f48c29dc49e7da592450e20
Davud <11396@holbertonstudents.com>
2025-06-23 22:27:04 +0400

    7th task v2

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -70,7 +70,7 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return ''
         else:
-            lines = [Rectangle.print_symbol * self.__width
+            lines = [self.print_symbol * self.__width
                      for _ in range(self.__height)]
             return "\n".join(lines)
 
