42f518d4c861cafb42224948b1b33945731a3d00
Davud <11396@holbertonstudents.com>
2025-06-25 02:41:54 +0400

    Last Advanced task v2

--- python-inheritance/101-add_attribute.py
+++ python-inheritance/101-add_attribute.py
@@ -2,6 +2,8 @@
 """Creating a function that adds attribute to class if it's possible."""
 
 
-def add_attribute(cls, attr_name, value):
-    """Add an attribute and value to class if possible."""
-    setattr(cls, attr_name, value)
+def add_attribute(obj, attr_name, value):
+    """Add an attribute and value to object if possible."""
+    if not hasattr(obj, "__dict__"):
+        raise TypeError("can't add new attribute")
+    setattr(obj, attr_name, value)
