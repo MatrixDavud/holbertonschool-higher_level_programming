32776a66e2e57e7d5d22df82c9957c422bd9d49d
Davud <11396@holbertonstudents.com>
2025-06-25 18:09:07 +0400

    0th task v2

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -4,6 +4,6 @@
 
 def read_file(filename=""):
     """Read file and print the contents."""
-    with open(filename, "r", "utf-8") as file:
+    with open(filename, "r", encoding="utf-8") as file:
         contents = file.read()
         print(contents)
