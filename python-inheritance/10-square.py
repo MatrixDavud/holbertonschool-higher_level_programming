4415574566aa593d6a71bf743c13717c65585cb3
Davud <11396@holbertonstudents.com>
2025-06-26 11:30:36 +0400

    10th task inheritance update v2

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -2,26 +2,7 @@
 """Script with Geometry class."""
 
 
-BaseGeometry = __import__('9-rectangle').BaseGeometry
-
-
-class Rectangle(BaseGeometry):
-    """Rectangle class inherited from BaseGeometry class."""
-
-    def __init__(self, width, height):
-        """Initialize rectangle with width and height and validate both."""
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        """Return rectangle area."""
-        return self.__height * self.__width
-
-    def __str__(self):
-        """Display Rectangle with its width and height."""
-        return f"[Rectangle] {self.__width}/{self.__height}"
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
