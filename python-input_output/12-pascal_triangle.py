c641048f9737e2495ee8dd885dccced4dce7d020
Davud <11396@holbertonstudents.com>
2025-06-26 13:09:16 +0400

    12th task v3

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -2,26 +2,14 @@
 """Pascal triangle module."""
 
 
-def factorial(n):
-    """Return the factorial of n with recursive function."""
-    if n == 0 or n == 1:
-        return 1
-    else:
-        return n * factorial(n-1)
-
-
-def n_choose_k(n, k):
-    """Return the value of C(n, k) for binomial coefficients."""
-    return int(factorial(n) / ((factorial(k)) * factorial(n-k)))
-
-
 def pascal_triangle(n):
     """Return list of lists for n size pascal triangle."""
-    final_pscl_lst = []
+    triangle = []
+
     for i in range(n):
-        crrnt_pscl_lst = []
-        for j in range(i+1):
-            bin_coeff = n_choose_k(i, j)
-            crrnt_pscl_lst.append(bin_coeff)
-        final_pscl_lst.append(crrnt_pscl_lst)
-    return final_pscl_lst
+        row = [1]
+        for j in range(1, i + 1):
+            val = row[j - 1] * (i - j + 1) // j
+            row.append(val)
+        triangle.append(row)
+    return triangle
