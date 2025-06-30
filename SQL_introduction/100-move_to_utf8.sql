2c3fc59563c77420b5c5f4ba0cf31dc8bc88d7de
Davud <11396@holbertonstudents.com>
2025-07-01 02:34:20 +0400

    Advanced task 1 MySQL v5

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -5,5 +5,10 @@ USE hbtn_0c_0;
 
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-ALTER TABLE first_table MODIFY name VARCHAR(256) COLLATE utf8mb4_unicode_ci;
+DROP TABLE IF EXISTS first_table;
 
+CREATE TABLE first_table (
+    id INT DEFAULT NULL,
+    name VARCHAR(256) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    score INT DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
