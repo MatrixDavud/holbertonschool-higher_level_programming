c9ad5feb876c15a67118e5898fece87a8da4b399
Davud <11396@holbertonstudents.com>
2025-07-01 13:20:13 +0400

    Advanced task 2

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,13 +1,9 @@
 -- Convert the database, table, and the field into utf8
--- Select the database first
 USE hbtn_0c_0;
 
--- Convert the database default charset and collation
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Convert the table charset and collation, including all text columns
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Specifically modify the 'name' field to set the collation only (no charset needed explicitly)
 ALTER TABLE first_table MODIFY name VARCHAR(256) COLLATE utf8mb4_unicode_ci DEFAULT NULL;
 
---
+++ SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,5 @@
+-- Show and group by average temperatures in descending order and the cities
+USE hbtn_0c_0;
+SELECT city, AVG(value) AS avg_temp
+FROM temperatures
+ORDER BY avg_temp DESC;
