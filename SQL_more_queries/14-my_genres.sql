ef1676a895d8a94a0f2634e12468bbc900ae79de
Davud <11396@holbertonstudents.com>
2025-07-02 11:47:36 +0400

    14th task v2

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,6 +1,7 @@
 -- Lists the genres of 'Dexter'
-SELECT tv_genres.name AS name
-FROM tv_genres
+SELECT tv_genres.name
+FROM tv_show_genres
+JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
 JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-WHERE tv_show_genres.show_id = ( SELECT tv_shows.id FROM tv_shows WHERE tv_shows.title = 'Dexter' )
-ORDER BY tv_genres.name ASC; 
+WHERE tv_shows.title = 'Dexter'
+ORDER BY tv_genres.name ASC;
